3329d59958d5d52bad995cf75b1a2345285c6f5f
Pankaj Sharma <310227165+devopsbypankaj@users.noreply.github.com>
2026-08-26 17:42:48 -0400

    feat: add subnet module and update virtual network

--- Modules/Azurerm_Resource_Group/Main.tf
+++ Modules/Azurerm_Resource_Group/Main.tf
@@ -1,10 +1,10 @@
 variable "rgs" {
-type = any  
+  type = any
 }
 
 
 resource "azurerm_resource_group" "rg-infra" {
   for_each = var.rgs
-name = each.value.name
-location = each.value.location
+  name     = each.value.name
+  location = each.value.location
 }
